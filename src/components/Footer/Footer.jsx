33d51a54d1960f5548677ed92c0b8f70b02ccefc
softsamiul <softsamiul.com>
2022-04-26 01:26:02 +0530

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,7 +18,10 @@ const Footer = () => {
                     <a href="#about">About</a>
                 </li>
                 <li>
-                    <a href="#portfolio">Skills & Experience</a>
+                    <a href="#experience">Skills & Experience</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
                 </li>
                 <li>
                     <a href="#contact">Contact</a>
