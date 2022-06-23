5cc3c6834e12cbb03a5ac6163c24d11d6b597de9
softsamiul <softsamiul.com>
2022-06-23 11:16:28 +0530

    nav icon of portfolio updated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,8 @@ import {AiOutlineHome} from 'react-icons/ai';
 import {AiOutlineUser} from 'react-icons/ai';
 import {BiBook} from 'react-icons/bi';
 import {BiMessageSquareDetail} from 'react-icons/bi';
-import {DiTechcrunch} from 'react-icons/di';
+// import {DiTechcrunch} from 'react-icons/di';
+import {RiTodoFill} from "react-icons/ri";
 import './Nav.css'
 
 const Nav = () => {
@@ -14,7 +15,7 @@ const Nav = () => {
             <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}> <AiOutlineHome /></a>
             <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}> <AiOutlineUser /></a>
             <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}> <BiBook /></a>
-            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}> <DiTechcrunch /></a>
+            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}> <RiTodoFill /></a>
             <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active': ''}> <BiMessageSquareDetail /></a>
         </nav>
     );
