e979025f334dc1b8c3c3acb60934ef72fcb2ce0f
Samiul Islam <softsamiul@gmail.com>
2023-02-01 13:49:42 +0530

    project data updated

--- src/Data/Projects_data.js
+++ src/Data/Projects_data.js
@@ -1,43 +1,50 @@
+import image1 from "../assets/portfolio1.png"
+import image2 from "../assets/portfolio2.png"
+import image3 from "../assets/portfolio3.png"
+import image4 from "../assets/portfolio4.png"
+import image5 from "../assets/portfolio5.png"
+import image6 from "../assets/portfolio6.png"
+
 const ProjectData = [
     {
         id: 1,
         title: 'Ease Your Trip—Travel/Tourism Web App',
-        thumbnail: 'https://i.ibb.co/f8B5GJq/1.png',
+        thumbnail: image1,
         github:'https://github.com/softsamiul/ease-your-trip-client',
         liveLink: 'https://react-burj-al-arab-54bca.web.app/'
     },
     {
         id:2,
         title: "MediCare's Kolkata—Healthcare Web App",
-        thumbnail: 'https://i.ibb.co/vPLgMsp/3.png',
+        thumbnail: image2,
         github:'https://github.com/softsamiul/medicares-kolkata',
         liveLink: 'https://medi-cares.web.app/'
     },
     {
         id: 3,
         title: 'EduMaster —Educational Web App',
-        thumbnail: 'https://i.ibb.co/PCfzcCG/4.png',
+        thumbnail: image3,
         github:'https://github.com/softsamiul/edumaster',
         liveLink: 'https://edumaster.netlify.app/'
     },
     {
         id: 4,
         title: 'FoodLand —Food Website',
-        thumbnail: 'https://i.ibb.co/Q9kf93F/6.png',
+        thumbnail: image4,
         github:'https://github.com/softsamiul/food-land',
         liveLink: 'https://softsamiul.github.io/food-land/'
     },
     {
         id: 5,
         title: 'Responsive Football -Sports Website',
-        thumbnail: 'https://i.ibb.co/R3TVTn4/2.png',
+        thumbnail: image5,
         github:'https://github.com/softsamiul/responsive-football',
         liveLink: 'https://softsamiul.github.io/responsive-football/'
     },
     {
         id: 6,
         title: 'React - React Redux To do app',
-        thumbnail: 'https://i.ibb.co/LJVhfdW/5.png',
+        thumbnail: image6,
         github:'https://github.com/softsamiul/xegggo-to-do-app',
         liveLink: 'https://xeggo-to-do.web.app/'
     },
